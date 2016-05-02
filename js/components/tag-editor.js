f723af98cc2f7ca81ba893f8516333c9f63346fe
Fábio Santos <fabiosantosart@gmail.com>
2016-05-02 23:51:22 +0100

    refactored tag editor

--- js/components/tag-editor.js
+++ js/components/tag-editor.js
@@ -7,19 +7,90 @@ import Relay from 'react-relay';
 import AddTagMutation from '../mutations/AddTagMutation';
 import EditTagMutation from '../mutations/EditTagMutation';
 
+function EditButton({ onEdit = () => null, children = 'Edit' }) {
+  return (
+    <button onClick={onEdit}>{children}</button>
+  );
+}
+
+function TagViewer({ tag, onEdit = () => null }) {
+  return (
+    <pre>
+      {tag.title}
+      <EditButton onEdit={onEdit} />
+    </pre>
+  )
+}
+
+class TagEditorEditMode extends React.Component {
+  isValid() {
+    if (!this.state) return false
+    const { title, source } = this.state;
+    return title && source
+  }
+  render() {
+    let {
+      tag,
+      onCancel = () => null,
+      onSave = () => null,
+      displayCancel,
+    } = this.props
+    tag = tag || {}
+    const editButton = displayCancel && (
+      <EditButton onEdit={onCancel}>Cancel</EditButton>
+    )
+    return (
+      <form>
+        <dl>
+          <dt> Title </dt>
+          <dl>
+            <input
+              ref={(input) => { this.refs = { ...this.refs, input } }}
+              defaultValue={tag.title}
+              onChange={ e => { this.setState({ title: e.target.value }) } }
+            />
+          </dl>
+          <dt> Source code </dt>
+          <dl>
+            <textarea
+              ref={(textarea) => { this.refs = { ...this.refs, textarea } } }
+              defaultValue={tag.source}
+              onChange={ e => { this.setState({ source: e.target.value }) } }
+            />
+          </dl>
+        </dl>
+        <button
+          disabled={!this.isValid()}
+          onClick={
+            (e) => {
+              e.preventDefault()
+              onSave({
+                title: this.state.title,
+                source: this.state.source,
+              })
+            }
+          }
+        >
+          Save
+        </button>
+        {editButton}
+      </form>
+    )
+  }
+}
+
 class TagEditor extends React.Component {
   componentWillMount() {
     if (this.props.createMode) {
       this.setState({ editMode: true })
     }
   }
-  handleSaveClick(e) {
+  initialiseMutation(fieldData) {
     const mutationVariables = {
+      ...fieldData,
       game: this.props.game,
       tag: this.props.tag,
       id: (this.props.tag||{}).id,
-      title: this.refs.input.value,
-      source: this.refs.textarea.value,
     }
     const mutation = this.props.createMode ?
       new AddTagMutation(mutationVariables) :
@@ -41,53 +112,22 @@ class TagEditor extends React.Component {
       editMode: !(this.state && this.state.editMode)
     })
   }
-  renderEditButton() {
-    const editWord = (this.state||{}).editMode ? 'Cancel' : 'Edit'
-    return (
-      <button onClick={() => { this.handleEditModeToggle() }}>{editWord}</button>
-    )
-  }
-  renderEditMode() {
-    const editButton =
-      this.props.createMode !== true ?  // If this was instantiated in create mode, don't toggle back
-        this.renderEditButton() :
-        null;
-    const tag = this.props.tag || {}
-    return (
-      <div>
-        <dl>
-          <dt> Title </dt>
-          <dl>
-            <input
-              ref={(input) => { this.refs = { ...this.refs, input } }}
-              defaultValue={tag.title}
-              onChange={ e => { this.setState({ title: e.target.value }) } }
-            />
-          </dl>
-          <dt> Source code </dt>
-          <dl>
-            <textarea
-              ref={(textarea) => { this.refs = { ...this.refs, textarea } } }
-              defaultValue={tag.source}
-              onChange={ e => { this.setState({ source: e.target.value }) } }
-            />
-          </dl>
-        </dl>
-        <button onClick={(e) => this.handleSaveClick(e)}>Save</button>
-        {editButton}
-      </div>
-    )
-  }
   render() {
     if (this.state && this.state.editMode) {
-      return this.renderEditMode()
+      return (
+        <TagEditorEditMode
+          tag={this.props.tag}
+          displayCancel={!this.props.createMode}
+          onCancel={ () => { this.handleEditModeToggle() } }
+          onSave={ (fieldData) => this.initialiseMutation(fieldData) }
+        />
+      )
     }
-    const { tag } = this.props
     return (
-      <pre>
-        {JSON.stringify(tag)}
-        {this.renderEditButton()}
-      </pre>
+      <TagViewer
+        tag={this.props.tag}
+        onEdit={ () => { this.handleEditModeToggle() } }
+      />
     )
   }
 }
